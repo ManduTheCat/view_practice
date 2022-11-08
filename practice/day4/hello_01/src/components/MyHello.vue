50e21fc39d6cbd51109971bb1dcb1a8622357388
ManduTheCat <krocd@naver.com>
2022-11-08 17:55:49 +0900

    backup

---
+++ practice/day4/hello_01/src/components/MyHello.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="my-hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  el: ".my-hello",
+  name: "MyHello",
+  props: {
+    msg: String,
+  },
+};
+</script>
